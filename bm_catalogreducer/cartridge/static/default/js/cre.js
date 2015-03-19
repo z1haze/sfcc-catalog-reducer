01f0257a5ae95344c555f036a8c336740a9a0bd0
Andrew Tougas <atougas@demandware.com>
2015-03-19 09:09:07 -0400

    Updates to CO definition. On form submit now passes the information into the custom object

--- bm_catalogreducer/cartridge/static/default/js/cre.js
+++ bm_catalogreducer/cartridge/static/default/js/cre.js
@@ -28,9 +28,36 @@
 			
 			jQuery("form#catalogreducerform").submit(function (e) {
 				e.preventDefault();
+				
+				if (jQuery("#onlineprods").prop('checked')) {
+					var onlineprods = true;
+				} 
+				else {
+					var onlineprods = false;
+				}
+				
+				var noofprods = jQuery("#noofprods").val();
+				if (noofprods === "") {
+					noofprods = 5;
+				}
+				else if (noofprods < 1) {
+					noofprods = 1;
+				}
+				else if (noofprods > 20) {
+					noofprods = 20;
+				}
+				
 				var $form = jQuery(this),
-					url = $form.attr('action');
-				cre.util.runCREJob(url);
+					url = $form.attr('action'),
+					prodids = jQuery("#prodids").val(),
+					expmethod = jQuery("#expmethod").val(),
+					data = {
+						noofprods: noofprods,
+						onlineprods: onlineprods,
+						prodids: prodids,
+						expmethod: expmethod
+					}
+				cre.util.runCREJob(url, data);
 			});
 		});
 	}
@@ -50,9 +77,10 @@
 				jQuery('#cre-menu-div').html($response);
 			});
 		},
-		runCREJob : function (url) {
-			var u = url;
-			jQuery.post(u).done(function(response) {
+		runCREJob : function (url, data) {
+			var u = url,
+				d = data;
+			jQuery.post(u, d).done(function(response) {
 				jQuery('#exportFinishMessageTbl').show();
 				var $response = jQuery(jQuery.trim(response));
 				jQuery('#exportFinishMessage').html($response);
