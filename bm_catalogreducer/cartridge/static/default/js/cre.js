8855f04b394b78ba9a8300d58831177df34b3722
Andrew Tougas <atougas@demandware.com>
2015-03-20 14:30:42 -0400

    Form validation for both master and storefront catalog selection

--- bm_catalogreducer/cartridge/static/default/js/cre.js
+++ bm_catalogreducer/cartridge/static/default/js/cre.js
@@ -58,6 +58,7 @@
 			
 			jQuery("form#catalogreducerform").submit(function (e) {
 				e.preventDefault();
+				var valid = false; // for form validation
 				
 				if (jQuery("#onlineprods").prop('checked')) {
 					var onlineprods = true;
@@ -77,18 +78,33 @@
 					noofprods = 20;
 				}
 				
-				var mastercat = "";
-				jQuery("input[name=mastercat]:checked").each(function() {
-					mastercat += $(this).val() + ",";
-				});
-				//remove last comma on master category list
-				mastercat = mastercat.slice(0, -1);
+				//check for master and storefront catalog selection
+				if ((jQuery("input[name=mastercat]:checked").length > 0) && (jQuery("input[name=storefrontcat]:checked").length > 0)) {
+					var mastercat = "";
+					jQuery("input[name=mastercat]:checked").each(function() {
+						mastercat += $(this).val() + ",";
+					});
+					//remove last comma on master category list
+					mastercat = mastercat.slice(0, -1);
+					
+					var storefrontcat = jQuery("input[name=storefrontcat]:checked").val();
+					jQuery("#catalogs-error").html('');
+					valid = true;
+				} else {
+					if ((jQuery("input[name=mastercat]:checked").length < 1) && (jQuery("input[name=storefrontcat]:checked").length < 1)) {
+						jQuery("#catalogs-error").html('Please select at least 1 master catalog and the storefront catalog!');
+					} else if (jQuery("input[name=mastercat]:checked").length < 1) {
+						jQuery("#catalogs-error").html('Please select at least 1 master catalog!');
+					} else if (jQuery("input[name=storefrontcat]:checked").length < 1) {
+						jQuery("#catalogs-error").html('Please select the storefront catalog!');
+					}
+				}
 				
 				var $form = jQuery(this),
 					url = $form.attr('action'),
 					prodids = jQuery("#prodids").val(),
 					expmethod = jQuery("#expmethod").val(),
-					storefrontcat = jQuery("input[name=storefrontcat]:checked").val(),
+					storefrontcat = storefrontcat,
 					data = {
 						noofprods: noofprods,
 						onlineprods: onlineprods,
@@ -97,7 +113,10 @@
 						mastercat: mastercat,
 						storefrontcat: storefrontcat
 					}
-				cre.util.runCREJob(url, data);
+				
+				if (valid) {
+					cre.util.runCREJob(url, data);
+				}
 			});
 		});
 	}
